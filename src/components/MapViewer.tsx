12456cbe589d4e7c5d0b47d4899f1ba81d426437
kazusan-baba <Desktop>
2024-11-16 12:46:58 +0900

    fix: Mapが表示されない不具合を修正
    スタイルで高さ・幅を指定していないとマップが表示されないため、各値を指定し、修正

--- src/components/MapViewer.tsx
+++ src/components/MapViewer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const MapViewer = () => {
   return (
     <div>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: "50vh", width: "100%" }}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
