0f84442ad3bce9f154238e6f9b9e873413c00437
kazusan-baba <Desktop>
2024-11-17 11:08:31 +0900

    fix: マーカーのアイコンが表示されない不具合を修正

--- src/components/MapViewer.tsx
+++ src/components/MapViewer.tsx
@@ -1,6 +1,19 @@
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import Leaflet from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerIconShadow from 'leaflet/dist/images/marker-shadow.png';
 import React from "react";
+
+const DefaultIcon = Leaflet.icon({
+  iconUrl : markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerIconShadow,
+  iconAnchor: [12, 41], // アイコンのオフセット
+  popupAnchor: [0, -32], // ポップアップのオフセット
+});
+Leaflet.Marker.prototype.options.icon = DefaultIcon;
 
 const MapViewer = () => {
   return (
