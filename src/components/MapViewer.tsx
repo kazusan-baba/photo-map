f826628594cfd6e1e6c59c928e420776017f009a
kazusan-baba <132738235+kazusan-baba@users.noreply.github.com>
2024-11-17 11:39:40 +0900

    feat/OpenStreetMap_Viewer (#1)
    * build: 地図表示用のライブラリを追加
    
    OpenStreetMapを表示するためにleafletと関連するReact用ライブラリを追加
    
    * feat: Sampleを参考にMap表示機能を追加
    
    * fix: Mapが表示されない不具合を修正
    
    スタイルで高さ・幅を指定していないとマップが表示されないため、各値を指定し、修正
    
    * fix: Mapの表示が崩れる不具合を修正
    
    leaflet.cssをインポートして修正
    
    * fix: マーカーのアイコンが表示されない不具合を修正
    
    * feat: Map内イベントの取得を実装
    
    クリック等のイベント情報を取得する関数を実装
    
    ---------
    
    Co-authored-by: kazusan-baba <Desktop>

---
+++ src/components/MapViewer.tsx
@@ -0,0 +1,46 @@
+import {MapContainer, Marker, Popup, TileLayer, useMapEvents} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import Leaflet from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerIconShadow from 'leaflet/dist/images/marker-shadow.png';
+import React from "react";
+
+const DefaultIcon = Leaflet.icon({
+  iconUrl : markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerIconShadow,
+  iconAnchor: [12, 41], // アイコンのオフセット
+  popupAnchor: [0, -32], // ポップアップのオフセット
+});
+Leaflet.Marker.prototype.options.icon = DefaultIcon;
+
+const MapViewer = () => {
+  const MapEvent = () => {
+    const map = useMapEvents({
+      click: (event) => {
+        console.log(event);
+      },
+    })
+    return null;
+  }
+  
+  return (
+    <div>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: "50vh", width: "100%" }}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+        <MapEvent/>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default MapViewer;
