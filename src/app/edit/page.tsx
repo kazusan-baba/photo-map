c53747fe0c335d8b516d2dfa395d9fd478d3c859
kazusan-baba <Desktop>
2024-12-11 09:47:15 +0900

    feat: 記事の編集画面を実装

---
+++ src/app/edit/page.tsx
@@ -0,0 +1,90 @@
+'use client';
+
+import {Button, TextField} from "@mui/material";
+import {LatLng, type LatLngLiteral} from "leaflet";
+import dynamic from "next/dynamic";
+import {useCallback, useMemo, useState} from "react";
+import {useDropzone} from "react-dropzone"
+import {Controller, useForm} from "react-hook-form";
+
+type EditForm = {
+  latitude: number;
+  longitude: number;
+  img: File;
+  description: string;
+}
+
+const Edit = () => {
+  const [location, setLocation] = useState<LatLngLiteral>(new LatLng(35.68, 139.76));
+  const [spots, setSpots] = useState<EditForm[]>([]);
+  const {control, handleSubmit, setValue} = useForm<EditForm>({});
+  
+  const MapComponent = useMemo(
+    () => dynamic(() => import("@/components/MapViewer"), {
+      loading: () => <p>A map loading</p>,
+      ssr: false,
+    }),
+    []
+  );
+  
+  const onMapClick = (point:LatLngLiteral) => {
+    setLocation(point);
+    setValue("latitude", point.lat);
+    setValue("longitude", point.lng);
+  };
+  
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    setValue("img", acceptedFiles[0]);
+  }, []);
+  
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+  
+  const onSubmit = (data: EditForm) => {
+    setSpots([...spots, data]);
+  };
+  
+  return (
+    <div>
+      <main>
+        <MapComponent location={location} onMapClick={onMapClick}/>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Controller
+            name="latitude"
+            control={control}
+            defaultValue={0}
+            render={({field}) => (
+              <TextField {...field} disabled={true} label={"緯度"}/>
+            )}
+          />
+          <Controller
+            name="longitude"
+            control={control}
+            defaultValue={0}
+            render={({field}) => (
+              <TextField {...field} disabled={true} label={"経度"}/>
+            )}
+          />
+          <Controller
+            name="description"
+            control={control}
+            defaultValue={""}
+            render={({field}) => (
+              <TextField {...field} placeholder={"説明"}/>
+            )}
+          />
+          <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            {
+              isDragActive ?
+                <p>Drop the files here ...</p> :
+                <p>画像を ドロップ または、 クリックして選択</p>
+            }
+          </div>
+          <Button type={"submit"}>追加</Button>
+        </form>
+      </main>
+    </div>
+  )
+}
+
+export default Edit;
