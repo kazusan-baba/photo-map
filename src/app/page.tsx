a70c62151e2f25fb60251a77f5848432c07bcf12
kazusan-baba <Desktop>
2024-12-11 10:28:02 +0900

    feat: 記事一覧を作成
    暫定的に仮データを配置

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,25 @@
+import Link from "next/link";
+
 const Home = () => (
   <div>
     <main>
+      <h2>フォトスポット一覧</h2>
+      <div>
+        <Link href={"/articles/0"}>
+          <h3>仮 タイトル</h3>
+          <div>
+            <img src={"/sample/download.jpg"} alt={"タイトルの写真"}/>
+            <p>詳細</p>
+          </div>
+        </Link>
+        <Link href={"/articles/1"}>
+          <h3>仮 タイトル</h3>
+          <div>
+            <img src={"/sample/download.jpg"} alt={"タイトルの写真"}/>
+            <p>詳細</p>
+          </div>
+        </Link>
+    </div>
     </main>
   </div>
 );
