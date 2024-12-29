ccb3be030da7fd706994d2fccf3fc7c38f357e4d
kazusan-baba <Desktop>
2024-12-29 19:04:21 +0900

    update: MUIでのデザインの実装
    MUIを用いて各コンポーネントを置き換え、スタイルを当てた

--- src/app/ArticleCard.tsx
+++ src/app/ArticleCard.tsx
@@ -1,4 +1,5 @@
 import { type Article, TEST_DATA } from "@/components/Article";
+import { Box, Divider, Link as MuiLink, Typography } from "@mui/material";
 import Link from "next/link";
 
 const ArticleCards = () => {
@@ -17,17 +18,22 @@ type ArticleCardProps = {
 
 const ArticleCard = ({ data }: ArticleCardProps) => {
 	return (
-		<div>
-			<Link href={`/article/${data.id}`}>
-				<h3>{data.title}</h3>
-			</Link>
-			<div>
+		<Box sx={{ boxShadow: 2, mb: 1 }}>
+			<MuiLink component={Link} href={`/article/${data.id}`} underline="hover">
+				<Typography variant={"h3"} sx={{ p: 1 }}>
+					{data.title}
+				</Typography>
+			</MuiLink>
+			<Divider variant={"middle"} />
+			<Box sx={{ p: 1, display: "flex", flexWrap: "wrap" }}>
 				<Link href={`/article/${data.id}`}>
 					<img src={data.thumbnail} alt={"タイトルの写真"} />
 				</Link>
-				<p>{data.description}</p>
-			</div>
-		</div>
+				<Typography variant={"body1"} sx={{ p: 1 }}>
+					{data.description}
+				</Typography>
+			</Box>
+		</Box>
 	);
 };
 
