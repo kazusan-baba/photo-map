92f68847b858d94b0ab3ad5fafe8bf85c3cf7893
kazusan-baba <Desktop>
2024-12-29 17:40:28 +0900

    feat: 記事IDに基づいて記事を表示
    クエリパラメータのIDを元に記事を取得し、その内容を出力。また、該当の記事がない際はNotFoundを返す

--- src/app/article/[id]/page.tsx
+++ src/app/article/[id]/page.tsx
@@ -1,19 +1,24 @@
+import {TEST_DATA} from "@/components/Article";
 import MapViewer from "@/components/MapViewer";
 import Link from "next/link";
+import {notFound} from "next/navigation";
 
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
 	const id = (await params).id;
 	const isAuthor = true;
+	
+	const article = TEST_DATA.find((article) => article.id === Number.parseInt(id));
+	if (!article) return notFound();
 
 	return (
 		<div>
-			<h2>仮タイトル</h2>
-			<img src={"/sample/download.jpg"} alt={""}/>
+			<h2>{article.title}</h2>
+			<img src={article.image[0]} alt={""}/>
 			{isAuthor && (<Link href={`/edit?id=${id}`}>編集</Link>)}
 			<p>記事ID:{id}</p>
 			<h3>紹介</h3>
-			<p>詳細</p>
-			<MapViewer/>
+			<p>{article.description}</p>
+			<MapViewer location={{lng: article.longitude, lat: article.latitude}}/>
 		</div>
 	);
 };
