679cff55474896f376681b835577356650e71a51
kazusan-baba <132738235+kazusan-baba@users.noreply.github.com>
2025-01-07 21:37:32 +0900

    update: MUIを用いてarticleページのレイアウトの実装 (#18)
    MUIを用いて各コンポーネントを置き換え、スタイルを当てた
    
    Co-authored-by: kazusan-baba <Desktop>

--- src/app/article/[id]/page.tsx
+++ src/app/article/[id]/page.tsx
@@ -1,5 +1,6 @@
 import {TEST_DATA} from "@/components/Article";
 import MapViewer from "@/components/MapViewer";
+import {Box, Divider, Link as MuiLink, Typography } from "@mui/material";
 import Link from "next/link";
 import {notFound} from "next/navigation";
 
@@ -11,15 +12,25 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
 	if (!article) return notFound();
 
 	return (
-		<div>
-			<h2>{article.title}</h2>
-			<img src={article.image[0]} alt={""}/>
-			{isAuthor && (<Link href={`/edit?id=${id}`}>編集</Link>)}
-			<p>記事ID:{id}</p>
-			<h3>紹介</h3>
-			<p>{article.description}</p>
-			<MapViewer location={{lng: article.longitude, lat: article.latitude}}/>
-		</div>
+		<Box sx={{p: 2}}>
+			<Box sx={{display: "flex",flexWrap: "nowrap", justifyContent: "space-between", alignItems: "baseline"}}>
+				<Typography variant={"h2"}>{article.title}</Typography>
+				{isAuthor && (<MuiLink component={Link} href={`/edit?id=${id}`} underline={"hover"}>編集</MuiLink>)}
+			</Box>
+			<Box sx={{display: "flex", justifyContent: "center", background: "#020202"}}>
+				<img src={article.image[0]} alt={""} style={{width: "75%"}}/>
+			</Box>
+			<Divider sx={{my: 2}}/>
+			<Box sx={{display: "flex", flexWrap: "wrap", justifyContent: "space-between"}}>
+				<Box>
+					<Typography variant={"h3"}>紹介</Typography>
+					<Typography>{article.description}</Typography>
+				</Box>
+				<Box sx={{width: "500px"}}>
+					<MapViewer location={{lng: article.longitude, lat: article.latitude}}/>
+				</Box>
+			</Box>
+		</Box>
 	);
 };
 export default Page;
