f676fc1538bc3cb0afc1babc3eb05fa295157fb6
kazusan-baba <132738235+kazusan-baba@users.noreply.github.com>
2025-01-07 21:34:37 +0900

    update: CreateArticle型のimageプロパティの変更 (#17)
    * update: 画像(image)プロパティの型を修正
    
    ファイルオブジェクトから、画像のパスを指定するように変更。
    
    * update: 画僧プロパティの型を修正
    
    オプション項目から必須項目に変更
    
    * fix: 画像プロパティ名を修正
    
    複数値が入ることが想定されることから、プロパティ名を複数形に修正
    
    ---------
    
    Co-authored-by: kazusan-baba <Desktop>

--- src/components/Article.ts
+++ src/components/Article.ts
@@ -2,7 +2,7 @@ type CreateArticle = {
   title: string,
   latitude: number;
   longitude: number;
-  image?: File[];
+  images: string[];
   description: string;
 }
 
@@ -18,15 +18,17 @@ const TEST_DATA: Article[] = [
     thumbnail: "/sample/download.jpg",
     latitude: 35.67164174274727,
     longitude: 139.78442788124087,
+    images: ["/sample/download.jpg"],
     description: "中央区佃にあるよ",
   },
   {
     id: 1,
     title: "中央大橋",
     thumbnail: "/sample/download.jpg",
-    description: "中央区新川にあるよ",
     latitude: 35.67164174274727,
     longitude: 139.78442788124087,
+    images: ["/sample/download.jpg"],
+    description: "中央区新川にあるよ",
   },
 ];
 
